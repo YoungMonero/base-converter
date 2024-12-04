d80f7377025dcad1c114548bb7e115582e8ee7c3
Young Monero <yungmonero29@gmail.com>
2024-12-04 10:11:45 +0100

    complete work

--- script/script.js
+++ script/script.js
@@ -1,21 +1,20 @@
 const inputBaseNumber = document.getElementById('basenumber')
 const convertButtom = document.getElementById('convertbutton')
 const result = document.getElementById('feedback')
-const restartButtom = document.getElementById('restartbutton')
+//const restartButtom = document.getElementById('restartbutton')
 const baseInput = document.getElementById('base')
 
 //funtion
 convertButtom.addEventListener('click', () => {
-    const convertBase = parseInt(inputBaseNumber.value, 10)
-    const baseInputNumber = parseInt(baseInput.value, 10)
-    if (isNaN(convertBase)){
-        result.textContent = 'please enter a valid base number'
-        result.style.color = 'red'
-        return
-    }
-        
-    const convertNumber = convertBase.toString(baseInputNumber)
+  const convertBase = parseInt(inputBaseNumber.value, 10)
+  const baseInputNumber = parseInt(baseInput.value, 10)
+  if (isNaN(convertBase)){
+    result.textContent = 'please enter a valid base number'
+    result.style.color = 'red'
+    return
+  }     
+  const convertNumber = convertBase.toString(baseInputNumber)
 
-    result.textContent = ` ${convertBase} to base ${baseInputNumber} is ${convertNumber}`
-    result.style.color = 'green'
+  result.textContent = ` ${convertBase} to base ${baseInputNumber} is ${convertNumber}`
+  result.style.color = 'green'
 })
